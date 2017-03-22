34eb2219009eef8d18eeb22fc319e2707884d119
liwenli <102495553@qq.com>
2017-03-22 18:52:21 +0800

    update

--- 第二周/Day2/utils.js
+++ 第二周/Day2/utils.js
@@ -1,6 +1,6 @@
 function listToArray(list) {
     try{
-        return [].slice.call(list, 0)
+        return [].slice.call(list, 0); // 如果不兼容则使用执行下面
     }catch (e){
         var arr = [];
         for(var i = 0; i < list.length; i++){
