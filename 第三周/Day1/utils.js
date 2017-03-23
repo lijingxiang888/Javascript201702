1837ee5a6b0d1c5bbc14987817c059cc35180fbf
liwenli <102495553@qq.com>
2017-03-23 18:12:48 +0800

    update

---
+++ 第三周/Day1/utils.js
@@ -0,0 +1,12 @@
+function listToArray(list) {
+    console.log(list);
+    try{
+        return [].slice.call(list, 0); // 如果不兼容则使用执行下面
+    }catch (e){
+        var arr = [];
+        for(var i = 0; i < list.length; i++){
+            arr.push(list[i]);
+        }
+        return arr;
+    }
+}
