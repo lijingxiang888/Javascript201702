0116cc6f7e2c48a7bb97dc1a4c0454dfe17e0f5a
lijingxiang888 <924415666@qq.com>
2017-03-28 19:13:47 +0800

    更新老师讲义

---
+++ 第二周/Day3/练手-表格排序/utilswo.js
@@ -0,0 +1,38 @@
+var utils ={
+    listToArray:function(list){
+        try{
+            return [].slice.call(list,0)
+        }catch (e){
+            var arr=[];
+            for (var i=0;i<list.length;i++){
+                arr.push(list[i]);
+            }
+            return arr
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
