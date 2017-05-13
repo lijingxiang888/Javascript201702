c0c3a1fa7ffff128dee81e33e6c2cb8c7e79b316
liwenli <102495553@qq.com>
2017-05-13 11:06:44 +0800

    update node+ajax

---
+++ 第九周/Day4/1.hello.js
@@ -0,0 +1,7 @@
+console.log('hello Node');
+
+
+let url = require('url');
+// function (exports, module.exports, __dirname, __filename) {
+//     console.log('hello Node');
+// }
---
+++ 第九周/Day4/modules.js
@@ -0,0 +1,15 @@
+// 搭建server的内置模块http
+let http = require('http');
+let vue = require('vue'); // 第三方模块
+// 第三方模块 引入查找机制
+
+// [ 'F:\\WEB2017Js\\Javascript201702\\第九周\\Day4\\node_modules',
+//     'F:\\WEB2017Js\\Javascript201702\\第九周\\node_modules',
+//     'F:\\WEB2017Js\\Javascript201702\\node_modules',
+//     'F:\\WEB2017Js\\node_modules',
+//     'F:\\node_modules' ]
+
+// console.log(http);
+// console.log(vue);
+
+console.log(module);
